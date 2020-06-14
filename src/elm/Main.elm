884c7a8c4debcc03d62cd29fac2f2dd8e90e8593
Phill Sparks <me@phills.me.uk>
2020-06-14 20:11:43 +0100

    Change player name

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -10,7 +10,7 @@ main : Program () Warrior.Model Warrior.Msg
 main =
     Warrior.program
         { maps = Maps.all
-        , players = [ ( "Player", Player.takeTurn ) ]
+        , players = [ ( "Phill", Player.takeTurn ) ]
         , msPerTurn = 500
         , progressionFunction = Progression.reachExitPoint
         }
